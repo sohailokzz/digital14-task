7521e51262ed64835ce13566141ccfe7629107ad
HP Computer s <sohailokzz@gmail.com>
2022-07-02 13:36:11 +0500

    Initial Project Set up

---
+++ lib/views/events_list_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EventsListView extends StatefulWidget {
+  const EventsListView({Key? key}) : super(key: key);
+
+  @override
+  State<EventsListView> createState() => _EventsListViewState();
+}
+
+class _EventsListViewState extends State<EventsListView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
